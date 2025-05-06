ee9e9107533407027df67fc88c0a7141bd29dd33
Toriq <107845795+garlicoo@users.noreply.github.com>
2025-05-06 10:02:54 +0700

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,35 +2,36 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Result = ({ name, score, resetScore, limit }) => {
+const Result = ({ name, nip, score, resetScore, limit }) => {
   const [saved, setSaved] = useState(false);
 
   useEffect(() => {
     if (!saved) {
       const saveResult = async () => {
         try {
-          const response = await fetch('https://api-backend-beige.vercel.app/results', {
+          const response = await fetch('http://localhost:5000/results', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ name, score })
+            body: JSON.stringify({ name, nip, score })
           });
           const data = await response.json();
           console.log('Result saved:', data);
-          setSaved(true); // Set the flag to true after the result is saved
+          setSaved(true);
         } catch (error) {
           console.error('Error saving result:', error);
         }
       };
       saveResult();
     }
-  }, [name, score, saved]);
+  }, [name, nip, score, saved]);
 
   return (
     <div className="container">
       <div className="result__container">
         <p className="result">Name: {name}</p>
+        <p className="result">Nip: {nip}</p>
         <p className="result">You Scored: {score} out of {limit}</p>
         <Link className="restart" to="/" onClick={resetScore}>Restart</Link>
       </div>
